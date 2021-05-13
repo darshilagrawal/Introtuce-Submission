7079890894685a45e7c748cffe1aa3d591d98b6b
darshilagrawal <darshilagrawal2000@gmail.com>
2021-05-13 23:45:02 +0530

    Implementation of Infinite Scrolling on TableView

--- Introtuce-Submission/Controller/ViewController.swift
+++ Introtuce-Submission/Controller/ViewController.swift
@@ -9,7 +9,7 @@ import UIKit
 import FirebaseDatabase
 
 class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
-    var total = 10
+    var total = 12
     var users=[RegisteredUser]()
     let tableView=UITableView()
     var ref:DatabaseReference?
@@ -20,8 +20,12 @@ class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell",for: indexPath)
         cell.textLabel?.text="Cell \(indexPath.row + 1)"
-        
+        cell.accessoryType = .disclosureIndicator
+        cell.backgroundColor = .cyan
         return cell
+    }
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 65.0
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
@@ -34,7 +38,14 @@ class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource
         }
     }
     
-    
+    func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        if indexPath.row == total-1{
+            total=total+12
+            DispatchQueue.main.async {
+                self.tableView.reloadData()
+            }
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         view.addSubview(tableView)
@@ -44,7 +55,7 @@ class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource
         ref=Database.database().reference()
         ref?.child("introtuce-submission-defualt-rtdb").observe(.value,with: { (snapshot) in
             print(snapshot.value as Any)
-
+            
         })
     }
     override func viewDidLayoutSubviews() {
